1ae0406ac1853a2e5555f881539aab2ee317e769
debugee <519306795@qq.com>
2023-07-25 20:02:25 +0800

    link.h

--- include/curl/curl_link.h
+++ include/curl/curl_link.h
@@ -2,7 +2,31 @@
 
 
 #ifdef _DEBUG
-#pragma comment(lib, "v12_smdd_libcurl.lib")
+	#ifdef _DLL
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smdd_libcurl.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smdd_libcurl.lib")
+		#endif
+	#else
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smtd_libcurl.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smtd_libcurl.lib")
+		#endif
+	#endif
 #else
-#pragma comment(lib, "v12_smd_libcurl.lib")
+	#ifdef _DLL
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smd_libcurl.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smd_libcurl.lib")
+		#endif
+	#else
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smt_libcurl.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smt_libcurl.lib")
+		#endif
+	#endif
 #endif
